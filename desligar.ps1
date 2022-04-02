759f2273949d051d5a6e85f5f1c55d4eae237de4
Teramanbr <viniciussms01@gmail.com>
2022-04-01 22:48:07 -0300

    Adição do inglês ao programa + melhora ENORME no README.md

--- desligar.ps1
+++ desligar.ps1
@@ -3,4 +3,4 @@
 $notify = new-object system.windows.forms.notifyicon
 $notify.icon = [System.Drawing.SystemIcons]::Information
 $notify.visible = $true
-$notify.showballoontip(10,"Success!"," Terabyte Tweaker has finished tweaking your computer! It will now restart in 5 minutes!",[system.windows.forms.tooltipicon]::None)
+$notify.showballoontip(10,"Sucesso!","Terabyte Tweaker acabou de terminar de turbinar seu computador, que vai reiniciar em 5 minutos!",[system.windows.forms.tooltipicon]::None)
---
+++ thanksforusing.bat
@@ -0,0 +1,34 @@
+@echo off
+color 6 
+echo. 
+echo. 
+echo.
+echo. 
+echo. 
+echo. 
+echo. 
+echo. 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                            Computer tweaked sucessfuly, enjoy the FPS that your computer needed.
+echo.                                                           UwU
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause
+start https://www.youtube.com/channel/UCIj-jcplOl9RqTfjXQ30JXA
+(goto) 2>nul & del "%~f0"

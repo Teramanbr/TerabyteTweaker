6b287b05fa6e459037c5d5c892b8a302bb1fd793
Teramanbr <viniciussms01@gmail.com>
2022-04-19 19:35:31 -0300

    Outra tentativa de fix nos tweaks do brawlhalla

--- src/Brawlhalla.ps1
+++ src/Brawlhalla.ps1
@@ -1,29 +1,29 @@
-(Get-Content localconfig.vdf) -replace '"291550"
-{
-    "LastPlayed"		"\d+"
-	"Playtime2wks"		"\d+"
-	"Playtime"		"\d+"
-	"BadgeData"		"\d+"
-	"\d+"				"\d+"
-}', '"291550"
-{
-	"LastPlayed"		"1650242448"
-	"Playtime2wks"		"902"
-	"Playtime"		"12027"
-	"BadgeData"		"020000000800"
-	"LaunchOptions"		"-unlockfps -framefixunbounded"
-}' | Out-File -encoding default localconfig.vdf
-(Get-Content localconfig.vdf) -replace '"291550"
-{
-    "LastPlayed"		"\d+"
-	"Playtime2wks"		"\d+"
-	"Playtime"		"\d+"
-	"BadgeData"		"\d+"
-}', '"291550"
-{
-	"LastPlayed"		"1650242448"
-	"Playtime2wks"		"902"
-	"Playtime"		"12027"
-	"BadgeData"		"020000000800"
-	"LaunchOptions"		"-unlockfps -framefixunbounded"
-}' | Out-File -encoding default localconfig.vdf
+(Get-Content localconfig.vdf) -replace '					"291550"
+					{
+						"LastPlayed"		"\d+"
+						"Playtime2wks"		"\d+"
+						"Playtime"		"\d+"
+						"BadgeData"		"\d+"
+						"LaunchOptions"		"\d+"
+					}', '					"291550"
+					{
+						"LastPlayed"		"\d+"
+						"Playtime2wks"		"\d+"
+						"Playtime"		"\d+"
+						"BadgeData"		"\d+"
+						"LaunchOptions"		"-unlockfps -framefixunbounded"
+					}' | Out-File -encoding default localconfig.vdf
+					(Get-Content localconfig.vdf) -replace '					"291550"
+					{
+						"LastPlayed"		"\d+"
+						"Playtime2wks"		"\d+"
+						"Playtime"		"\d+"
+						"BadgeData"		"\d+"
+					}', '					"291550"
+					{
+						"LastPlayed"		"\d+"
+						"Playtime2wks"		"\d+"
+						"Playtime"		"\d+"
+						"BadgeData"		"\d+"
+						"LaunchOptions"		"-unlockfps -framefixunbounded"
+					}' | Out-File -encoding default localconfig.vdf

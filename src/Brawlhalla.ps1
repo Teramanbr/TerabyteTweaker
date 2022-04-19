e304efeacede910fdec2af42d5ea05f3a9321af1
Teramanbr <viniciussms01@gmail.com>
2022-04-18 22:05:52 -0300

    Brawlhalla Fix Tentativa 9001

--- src/Brawlhalla.ps1
+++ src/Brawlhalla.ps1
@@ -4,7 +4,21 @@
 	"Playtime2wks"		"\d+"
 	"Playtime"		"\d+"
 	"BadgeData"		"\d+"
-	\d+				"\d+"
+	"\d+"				"\d+"
+}', '"291550"
+{
+	"LastPlayed"		"1650242448"
+	"Playtime2wks"		"902"
+	"Playtime"		"12027"
+	"BadgeData"		"020000000800"
+	"LaunchOptions"		"-unlockfps -framefixunbounded"
+}' | Out-File -encoding default localconfig.vdf
+(Get-Content localconfig.vdf) -replace '"291550"
+{
+    "LastPlayed"		"\d+"
+	"Playtime2wks"		"\d+"
+	"Playtime"		"\d+"
+	"BadgeData"		"\d+"
 }', '"291550"
 {
 	"LastPlayed"		"1650242448"
